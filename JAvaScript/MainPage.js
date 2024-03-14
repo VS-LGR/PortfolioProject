87320ef35e648d646ee6072a3dfc71b160d33747
Lucas Gabriel Rodrigues <rm556129@FIAP.COM.BR>
2024-03-14 17:33:22 -0300

    Adicionar caixa de texto expansiva - falta customizar

--- JAvaScript/MainPage.js
+++ JAvaScript/MainPage.js
@@ -1,11 +1,3 @@
-var scale = document.getElementById("content").style.scale
-var width = document.getElementById("content").style.width
-var height = document.getElementById("content").style.height
-
-function EscalaResponsiva(){
-
-    if(width && height < 10){
-      scale = 0.4;
-    }
-
+function expandTextarea(element) {
+  element.classList.toggle("expanded");
 }
